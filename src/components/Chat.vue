7480d83d04bc4fcf2914136f26a73d7a5f503ecc
Adam <adamorlowskipoland@icloud.com>
2018-05-14 01:30:11 +0200

    add new message component

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,12 +1,31 @@
 <template>
     <div class="chat container">
-        <h2>Chat as {{ this.name }}</h2>
+        <h2 class="center teal-text">Life Chatting as {{ this.name }}</h2>
+    <div class="card">
+      <div class="card-content">
+          <ul class="messages">
+              <li>
+                  <span class="teal-text">Name</span>
+                  <span class="grey-text text-darken-3">Message</span>
+                  <span class="grey-text time">Time</span>
+              </li>
+          </ul>
+      </div>
+      <div class="card-action">
+          <new-message :name='name'/>
+      </div>
+    </div>
     </div>
 </template>
 
 <script>
+import NewMessage from '@/components/NewMessage'
+
 export default {
   name: 'Chat',
+  components: {
+      NewMessage,
+  },
   props: {
     name: {
       type: String,
@@ -20,5 +39,15 @@ export default {
 </script>
 
 <style>
-
+.chat h2 {
+    font-size: 2.6em;
+    margin-bottom: 40px;
+}
+.chat span {
+    font-size: 1.4em;
+}
+.chat .time {
+    display: block;
+    font-size: 1.2em;
+}
 </style>
---
+++ src/components/NewMessage.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="new-message">
+    <form @submit.prevent="addMessage">
+        <label for="newMessage">New message (enter to add)</label>
+        <input type="text"
+            name="newMessage"
+            id="newMessage"
+            v-model="newMessage">
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NewMessage',
+  props: {
+    name: {
+      type: String,
+      required: true,
+    }
+  },
+  data() {
+    return {
+      newMessage: null,  
+    };
+  },
+  methods: {
+    addMessage() {
+      console.log(this.newMessage);
+      console.log(this.name);
+      console.log(Date.now());
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
