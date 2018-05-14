a1f1d58674104929f49dbb48e4c7ad8a98a9d2a9
Adam <adamorlowskipoland@icloud.com>
2018-05-14 22:39:16 +0200

    store msgs in firebase

--- src/components/NewMessage.vue
+++ src/components/NewMessage.vue
@@ -5,12 +5,17 @@
         <input type="text"
             name="newMessage"
             id="newMessage"
+            autocomplete="off"
             v-model="newMessage">
+            <p v-if="feedback"
+                class="red-text">{{ feedback }}</p>
     </form>
   </div>
 </template>
 
 <script>
+import db from '@/firebase/init';
+
 export default {
   name: 'NewMessage',
   props: {
@@ -22,13 +27,25 @@ export default {
   data() {
     return {
       newMessage: null,
+      feedback: null,
     };
   },
   methods: {
     addMessage() {
-      console.log(this.newMessage);
-      console.log(this.name);
-      console.log(Date.now());
+      if (this.newMessage) {
+        db.collection('messages').add({
+          content: this.newMessage,
+          name: this.name,
+          timestamp: Date.now(),
+        })
+          .catch((err) => {
+            console.error(err);
+          });
+        this.newMessage = null;
+        this.feedback = null;
+      } else {
+        this.feedback = 'Enter a message you want to sand';
+      }
     },
   },
 };
