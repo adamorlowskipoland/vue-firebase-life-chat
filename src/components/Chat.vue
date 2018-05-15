537027c65e0cb89663f11325dd24ecb98c3504dd
Adam <adamorlowskipoland@icloud.com>
2018-05-15 19:35:57 +0200

    display date in format using moment js

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,27 +1,31 @@
 <template>
-    <div class="chat container">
-      <h2 class="center teal-text">Life Chatting as {{ this.name }}</h2>
-      <div class="card">
-        <div class="card-content">
-          <ul class="messages">
-            <li v-for="message in messages"
-                :key="message.id">
-              <span class="teal-text">{{ message.name }}</span>
-              <span class="grey-text text-darken-3">{{ message.content }}</span>
-              <span class="grey-text time">{{ message.timestamp }}</span>
-            </li>
-          </ul>
-        </div>
-        <div class="card-action">
-          <new-message :name='name'/>
-        </div>
+  <div class="chat container">
+    <h2 class="center teal-text">Life Chatting as {{ this.name }}</h2>
+    <div class="card">
+      <div class="card-content">
+        <ul class="messages">
+          <li v-for="message in messages"
+              :key="message.id">
+            <span class="teal-text">{{ message.name }}</span>
+            <span class="grey-text text-darken-3">{{ message.content }}</span>
+            <span class="grey-text time">{{ message.timestamp }}</span>
+          </li>
+        </ul>
+      </div>
+      <div class="card-action">
+        <new-message :name='name'/>
       </div>
     </div>
+  </div>
 </template>
 
 <script>
 import NewMessage from '@/components/NewMessage';
 import db from '@/firebase/init';
+import moment from 'moment';
+
+// const lang = navigator.language.slice(0, 2);
+// moment.locale(lang);
 
 export default {
   name: 'Chat',
@@ -50,7 +54,7 @@ export default {
             id: doc.id,
             name: doc.data().name,
             content: doc.data().content,
-            timestamp: doc.data().timestamp,
+            timestamp: moment(doc.data().timestamp).format('LLLL'),
           });
         }
       });
@@ -60,15 +64,18 @@ export default {
 </script>
 
 <style>
-.chat h2 {
-  font-size: 2.6em;
-  margin-bottom: 40px;
-}
-.chat span {
-  font-size: 1.4em;
-}
-.chat .time {
-  display: block;
-  font-size: 1.2em;
-}
+  .chat h2 {
+    font-size: 2.6em;
+    margin-bottom: 40px;
+  }
+  .chat span {
+    font-size: 1.4em;
+  }
+  .chat .time {
+    display: block;
+    font-size: 0.8em;
+  }
+  .messages li {
+    margin-bottom: 5px;
+  }
 </style>
